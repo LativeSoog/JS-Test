5bb8c4fba2a35886d3e927b2de9c751afd56566a
Vitaliy <konfet88@gmail.com>
2023-03-04 00:37:14 +0600

    Correct index.html Added code hw-2.js

---
+++ js/hw-2.js
@@ -0,0 +1,53 @@
+/*
+//Задача 1
+let a = 20;
+alert(a);
+
+//Задача 2
+let yearOneIphone = 2007
+alert(yearOneIphone);
+
+//Задача 3
+let nameDevJs = 'Брендан Айком';
+alert(nameDevJs);
+
+//Задача 4
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+//Задача 5
+let d = 2;
+let result = (d ** 5);
+alert(result);
+
+*/
+
+//Задача 6
+let e = 9;
+let f = 2;
+let resultDivision = e % f;
+alert(resultDivision);
+
+//Задача 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Задача 8
+let age = String(prompt(`Сколько вам лет?`));
+alert(`Вам ${age} лет`);
+
+//Задача 9
+
+//Задача 10
+let nameUser = String(prompt(`Укажите ваше имя`));
+alert(`Привет, ${nameUser}!`);
