006de0c2df29a3126d9a57a226b19db9736e7f3f
Vitaliy <konfet88@gmail.com>
2023-03-10 22:31:53 +0600

    Added task 5, 6

--- js/hw-4.js
+++ js/hw-4.js
@@ -1,3 +1,4 @@
+
 // Задание 1
 
 let helloNumber = 1;
@@ -25,3 +26,31 @@ while (b <= 22) {
 }
 
 //Задание 4
+
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+};
+
+for (let salaryPerson in obj) {
+    console.log(`${salaryPerson} - зарплата ${obj[salaryPerson]} долларов`);
+}
+
+//Задание 5
+
+let num = 0;
+
+for (let n = 1000; n >= 50; num++) {
+    n /= 2;
+    console.log(n);
+    console.log(num);
+}
+
+// Задание 6
+
+const firstFridayMonth = 5;
+
+for (let toDay = firstFridayMonth; toDay < 31; toDay += 7) {
+    console.log(`Сегодня пятница, ${toDay}-е число. Необходимо подготовить отчёт.`);
+}
